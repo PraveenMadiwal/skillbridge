b293a2caded5f3e46e71ce51c3ba5fce8ee22ca0
PraveenMadiwal <praveenpavi633@gmail.com>
2026-05-09 16:48:31 +0530

    some changes done

--- skillbridge-frontend/src/services/api.js
+++ skillbridge-frontend/src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+//  server of backend that everything will be sent to, and the token is added to the header of every request if it exists in local storage. This allows for authenticated requests to be made to the backend without having to manually add the token to each request.
 const API = axios.create({
   baseURL: "https://skillbridge-lq49.onrender.com/api",
 });
@@ -15,7 +15,7 @@ API.interceptors.request.use(
 
     return req;
   },
-  (error) => Promise.reject(error)
+  (error) => Promise.reject(error) 
 );
 
 export default API;
